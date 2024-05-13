cc60615b19c8c26a90ed2f0065475b1553c3aa7e
Gabriel Ribeiro <133721315+Gbrribeiro30@users.noreply.github.com>
2024-05-13 12:03:32 -0300

    Colocado o automatizador

--- msg-py.py
+++ msg-py.py
@@ -1,30 +1,60 @@
 import pyautogui as pa #pip install pyautogui
 import time
 import pyperclip
+import schedule #pip install schedule
 
-contato= 'Irmão'
-pyperclip.copy(contato)
+contato= 'Paizão'
 
 wpp= 'whats'
 
-#abrir o wpp
-pa.press('super')
-time.sleep(0.5)
-pa.write(wpp)
-pa.press('enter')
-time.sleep(1)
-#buscar contato
-pa.hotkey("ctrl", "f")
-pa.hotkey("ctrl", "v")
-time.sleep(1)
-#mandar msg
-msg = 'Teste'
-pyperclip.copy(msg)
-pa.click(242, 190)
-time.sleep(1)
-i = 0
-while i < 2:
+def manha(): 
+    #abrir o wpp
+    pa.press('super')
+    time.sleep(0.5)
+    pa.write(wpp)
+    pa.press('enter')
+    time.sleep(1)
+    #buscar contato
+    pyperclip.copy(contato)
+    pa.hotkey("ctrl", "f")
     pa.hotkey("ctrl", "v")
+    time.sleep(1)
+    pa.click(242, 190)
+    #mandar msg
+    msg = '(Mensagem automatica) Bom dia Pai tudo bem? Espero que sim, por favor não esquece dos gatos, beijo te amo'
+    pyperclip.copy(msg)
+    time.sleep(1)
+    pa.hotkey("ctrl", "v")
+    time.sleep(0.5)
     pa.hotkey('enter')
     time.sleep(0.5)
-    i = i+1
+
+def tarde():        
+
+    #abrir o wpp
+    pa.press('super')
+    time.sleep(0.5)
+    pa.write(wpp)
+    pa.press('enter')
+    time.sleep(1)
+    #buscar contato
+    pyperclip.copy(contato)
+    pa.hotkey("ctrl", "f")
+    pa.hotkey("ctrl", "v")
+    time.sleep(1)
+    pa.click(242, 190)
+    #mandar msg
+    msg = '(Mensagem automatica) Boa tarde Pai tudo bem? Espero que sim, por favor não esquece dos gatos, beijo te amo'
+    pyperclip.copy(msg)
+    time.sleep(1)
+    pa.hotkey("ctrl", "v")
+    time.sleep(0.5)
+    pa.hotkey('enter')
+    time.sleep(0.5)
+
+schedule.every().day.at("06:30").do(manha)
+schedule.every().day.at("17:00").do(tarde)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
