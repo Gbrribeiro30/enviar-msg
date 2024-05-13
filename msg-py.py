2834c14f9d4e67d0a81f4fa55be7f8c7aac0bcb5
Gabriel Ribeiro <133721315+Gbrribeiro30@users.noreply.github.com>
2024-05-13 10:31:32 -0300

    Executavel
    Feita alterações para virar executavel

--- msg-py.py
+++ msg-py.py
@@ -1,9 +1,12 @@
 import pyautogui as pa #pip install pyautogui
 import time
-import pyperclip
+import pyperclip as py
 
-contato= 'Irmão'
-pyperclip.copy(contato)
+contato = input('Escreva o contato do mesmo jeito que está salvo no wpp:')
+py.copy(contato)
+msg = input('Escreva a mensagem desejada:')
+
+quant = int (input('Quantas mensagem quer enviar(número inteiro):'))
 
 wpp= 'whats'
 
@@ -18,12 +21,11 @@ pa.hotkey("ctrl", "f")
 pa.hotkey("ctrl", "v")
 time.sleep(1)
 #mandar msg
-msg = 'Teste'
-pyperclip.copy(msg)
+py.copy(msg)
 pa.click(242, 190)
 time.sleep(1)
 i = 0
-while i < 2:
+while i < quant:
     pa.hotkey("ctrl", "v")
     pa.hotkey('enter')
     time.sleep(0.5)
